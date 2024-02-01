9a6b590e5bbc90d88e2d26b236e89c4b64bd66a7
tanishkh16 <tanishkhandelwaldsa16@gmail.com>
2024-02-01 17:53:00 +0530

    changes done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                          <a href="https://flowbite.com/" class="hover:underline">ShowVista</a>
                       </li>
                       <li>
                           <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
